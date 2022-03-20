60bc4e5821b32b14b1b48f763c656b2557075801
LHayer-UOC <lhayer@uoc.edu>
2022-03-20 19:45:58 +0100

    add custom button

--- app/views/Home/Home.js
+++ app/views/Home/Home.js
@@ -1,18 +1,26 @@
-import React from "react";
-import { Text } from "react-native";
-import { View } from "react-native";
-import Container from "../../widgets/shared/Container/Container";
+import React from 'react';
+import { View, Text, Alert } from 'react-native';
+import Container from '../../widgets/shared/Container/Container';
+import CustomButton from '../../widgets/shared/Button/CustomButton';
+import RocketSvg from '../../../assets/rocket.svg';
 
-import { homeStyles } from "./HomeStyles";
+import { homeStyles } from './HomeStyles';
 
 const Home = () => {
-  return (
-    <Container>
-      <View>
-        <Text>¡Cargaste la home!</Text>
-      </View>
-    </Container>
-  );
+	const showAlert = () => {
+		Alert.alert('Botón pulsado', 'Has pulsado el botón ...');
+	};
+
+	return (
+		<Container>
+			<View>
+				<Text>¡Cargaste la home!</Text>
+				<CustomButton title={'Ver todos tus retos'} action={showAlert}>
+					<RocketSvg width="16" height="16" />
+				</CustomButton>
+			</View>
+		</Container>
+	);
 };
 
 export default Home;
