974096d3354ad6bd64e57be5e874c776ba4164cf
Ferran <ferran.puigmar@gmail.com>
2022-03-22 18:22:47 +0100

    Fix layout

--- app/views/Home/Home.js
+++ app/views/Home/Home.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import { View, Text, Alert } from 'react-native';
+import React from "react";
+import { View, Alert } from "react-native";
 
-import Container from '../../widgets/shared/Container/Container';
-import CustomButton from '../../widgets/shared/Button/CustomButton';
-import LastChallengeCard from '../../widgets/home/LastChallengeCard/LastChallengeCard';
+import Container from "../../widgets/shared/Container/Container";
+import CustomButton from "../../widgets/shared/Button/CustomButton";
+import LastChallengeCard from "../../widgets/home/LastChallengeCard/LastChallengeCard";
+import RocketSvg from "../../../assets/rocket.svg";
 
-import RocketSvg from '../../../assets/rocket.svg';
-
-import { homeStyles } from './HomeStyles';
+import { homeStyles } from "./HomeStyles";
 
 const Home = () => {
-	const showAlert = () => {
-		Alert.alert('Botón pulsado', 'Has pulsado el botón ...');
-	};
+  const showAlert = () => {
+    Alert.alert("Botón pulsado", "Has pulsado el botón ...");
+  };
 
-	return (
-		<Container>
-			<View>
-				<Text>¡Cargaste la home!</Text>
-				<LastChallengeCard />
-				<CustomButton title={'Ver todos tus retos'} action={showAlert}>
-					<RocketSvg width="16" height="16" fill="#FFF" />
-				</CustomButton>
-			</View>
-		</Container>
-	);
+  return (
+    <Container>
+      <View style={homeStyles.screenContainer}>
+        <View style={homeStyles.lastChallengeBlock}>
+          <LastChallengeCard />
+          <CustomButton title={"Ver todos tus retos"} action={showAlert}>
+            <RocketSvg width="16" height="16" fill="#FFF" />
+          </CustomButton>
+        </View>
+      </View>
+    </Container>
+  );
 };
 
 export default Home;
--- app/views/Home/HomeStyles.js
+++ app/views/Home/HomeStyles.js
@@ -1,3 +1,13 @@
 import { StyleSheet } from "react-native";
 
-export const homeStyles = StyleSheet.create({});
+export const homeStyles = StyleSheet.create({
+  screenContainer: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    paddingBottom: 20,
+  },
+  lastChallengeBlock: {
+    marginTop: "auto",
+  },
+});
