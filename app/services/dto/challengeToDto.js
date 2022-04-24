69f327b2ec9745f6328a63c6a2edde3844b7b6f4
LHayer-UOC <lhayer@uoc.edu>
2022-04-24 15:27:38 +0200

    changes in challenge detail

--- app/services/dto/challengeToDto.js
+++ app/services/dto/challengeToDto.js
@@ -1,10 +1,11 @@
-export const challengeToDto = (data) => {
-  return {
-    id: data.id,
-    percentage: data.percentage ?? 0,
-    title: data.title ?? "",
-    description: data.description ?? "",
-    periodicity: data.periodicity,
-    category: data.category,
-  };
+export const challengeToDto = data => {
+	return {
+		id: data.id,
+		percentage: data.percentage ?? 0,
+		title: data.title ?? '',
+		description: data.description ?? '',
+		periodicity: data.periodicity,
+		time: data.time ?? 0,
+		category: data.category,
+	};
 };
