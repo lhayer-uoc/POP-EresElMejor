5f1251be3cbfe41f1a979aaf1d5bf147e15889a5
LHayer-UOC <lhayer@uoc.edu>
2022-05-07 00:03:49 +0200

    fix save categories

--- app/views/NewChallenge/NewChallenge.js
+++ app/views/NewChallenge/NewChallenge.js
@@ -1,146 +1,148 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useCallback } from 'react';
 import {
-  View,
-  Text,
-  TouchableOpacity,
-  Keyboard,
-  TouchableWithoutFeedback,
-  Button
-} from "react-native";
+	View,
+	Text,
+	TouchableOpacity,
+	Keyboard,
+	TouchableWithoutFeedback,
+} from 'react-native';
 
-import { newChallengeStyles } from "./NewChallengeStyles";
-import { setChallengeService } from "../../services/setNewChallenge";
-import CustomInput from "../../widgets/shared/CustomInput/CustomInput";
-import { useForm } from "../../hooks/useForm";
-import { useFocusEffect } from "@react-navigation/native";
-import { emptyField, onlyNumbers } from "../../utils/formValidations";
-import { ScrollView } from "react-native-gesture-handler";
-import { showMessage } from "react-native-flash-message";
-import { useKeyboardStatus } from "../../hooks/useKeyboardStatus";
-import { useState } from "react";
-import { getCategoriesService } from "../../services/getCategoriesService";
-
+import { newChallengeStyles } from './NewChallengeStyles';
+import { setChallengeService } from '../../services/setNewChallenge';
+import CustomInput from '../../widgets/shared/CustomInput/CustomInput';
+import { useForm } from '../../hooks/useForm';
+import { useFocusEffect } from '@react-navigation/native';
+import { emptyField, onlyNumbers } from '../../utils/formValidations';
+import { ScrollView } from 'react-native-gesture-handler';
+import { showMessage } from 'react-native-flash-message';
+import { useKeyboardStatus } from '../../hooks/useKeyboardStatus';
+import { useState } from 'react';
+import { getCategoriesService } from '../../services/getCategoriesService';
 
 const NewChallenge = () => {
-  const [category, setCategory] = useState([{ name: "", value: "" }]);
-  const isKeyboardShown = useKeyboardStatus();
+	const [categories, setCategories] = useState([]);
+	const isKeyboardShown = useKeyboardStatus();
 
-  const {
-    title,
-    description,
-    periodicity,
-    time,
-    onChange,
-    onBlur,
-    form,
-    getFormParams,
-    resetForm,
-  } = useForm();
+	const {
+		title,
+		description,
+		periodicity,
+		time,
+		category,
+		onChange,
+		onChangeSelect,
+		onBlur,
+		form,
+		getFormParams,
+		resetForm,
+	} = useForm();
 
-  const handleSubmitForm = async () => {
-    try {
-      await setChallengeService(
-        title,
-        description,
-        time,
-        category,
-        periodicity
-      );
-      showMessage({
-        message: "Tu reto se ha creado correctamente",
-        type: "success",
-      });
-      resetForm();
-    } catch (error) {
-      console.log("error: ", error);
-      showMessage({
-        message: "Ha ocurrido un error, vuelve a intentarlo",
-        type: "error",
-      });
-    }
-  };
+	const handleSubmitForm = async () => {
+		try {
+			await setChallengeService(
+				title,
+				description,
+				time,
+				category,
+				periodicity
+			);
+			showMessage({
+				message: 'Tu reto se ha creado correctamente',
+				type: 'success',
+			});
+			resetForm();
+		} catch (error) {
+			console.log('error: ', error);
+			showMessage({
+				message: 'Ha ocurrido un error, vuelve a intentarlo',
+				type: 'error',
+			});
+		}
+	};
 
-  const handleChangeSelect = async () => {
-    const categories = await getCategoriesService();
-    setCategory(categories);
-  }
+	const handleChangeSelect = async () => {
+		const categories = await getCategoriesService();
+		setCategories(categories);
+	};
 
-  useFocusEffect(useCallback(() => {
-    handleChangeSelect();
-  }, []));
+	useFocusEffect(
+		useCallback(() => {
+			handleChangeSelect();
+		}, [])
+	);
 
-  useFocusEffect(() => {
-    if (!form) {
-      getFormParams({
-        title: { value: "", validation: [emptyField] },
-        description: { value: "", validation: [emptyField] },
-        periodicity: { value: "", validation: [emptyField] },
-        time: { value: "", validation: [emptyField, onlyNumbers] },
-        category: { value: "", validation: [emptyField, onlyNumbers] },
-      });
-    }
-  });
+	useFocusEffect(() => {
+		if (!form) {
+			getFormParams({
+				title: { value: '', validation: [emptyField] },
+				description: { value: '', validation: [emptyField] },
+				periodicity: { value: '', validation: [emptyField] },
+				time: { value: '', validation: [emptyField, onlyNumbers] },
+				category: { value: '', validation: [emptyField, onlyNumbers] },
+			});
+		}
+	});
 
-  return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <ScrollView>
-        <View style={newChallengeStyles.container}>
-          <CustomInput
-            placeholder="Escribe el nombre del reto"
-            label="Titulo"
-            value={title?.value}
-            onChange={(value) => onChange(value, "title")}
-            onBlur={() => onBlur("title")}
-            error={title?.errorMessage}
-            labelAlign="center"
-          />
-          <CustomInput
-            placeholder="Escribe la descripcion del reto"
-            label="Descripcion"
-            value={description?.value}
-            onChange={(value) => onChange(value, "description")}
-            onBlur={() => onBlur("description")}
-            error={description?.errorMessage}
-            labelAlign="center"
-          />
-          <CustomInput
-            placeholder="Escribe la categoria del reto"
-            label="Categoria"
-            type="select"
-            selectValue={category}
-            onChangeSelect={handleChangeSelect}
-            labelAlign="center"
-            options={category}
-          />
-          <CustomInput
-            placeholder="Escribe el número de días que durará el reto"
-            label="Duración"
-            value={time?.value}
-            onChange={(value) => onChange(value, "time")}
-            onBlur={() => onBlur("time")}
-            error={time?.errorMessage}
-            labelAlign="center"
-          />
-          <CustomInput
-            placeholder="Escribe cuantos días le dedicaras a la semana"
-            label="Periodicidad"
-            value={periodicity?.value}
-            onChange={(value) => onChange(value, "periodicity")}
-            onBlur={() => onBlur("periodicity")}
-            error={periodicity?.errorMessage}
-            labelAlign="center"
-          />
-          {!isKeyboardShown && (
-            <TouchableOpacity
-              style={newChallengeStyles.button}
-              onPress={handleSubmitForm}
-            >
-              <Text style={newChallengeStyles.textButton}> Guardar reto</Text>
-            </TouchableOpacity>
-          )}
-        </View>
-      </ScrollView>
-    </TouchableWithoutFeedback>
-  );
+	return (
+		<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+			<ScrollView>
+				<View style={newChallengeStyles.container}>
+					<CustomInput
+						placeholder="Escribe el nombre del reto"
+						label="Titulo"
+						value={title?.value}
+						onChange={value => onChange(value, 'title')}
+						onBlur={() => onBlur('title')}
+						error={title?.errorMessage}
+						labelAlign="center"
+					/>
+					<CustomInput
+						placeholder="Escribe la descripcion del reto"
+						label="Descripcion"
+						value={description?.value}
+						onChange={value => onChange(value, 'description')}
+						onBlur={() => onBlur('description')}
+						error={description?.errorMessage}
+						labelAlign="center"
+					/>
+					<CustomInput
+						placeholder="Escribe la categoria del reto"
+						label="Categoria"
+						type="select"
+						selectValue={category?.value}
+						onChangeSelect={value => onChangeSelect(value, 'category')}
+						labelAlign="center"
+						options={categories}
+					/>
+					<CustomInput
+						placeholder="Escribe el número de días que durará el reto"
+						label="Duración"
+						value={time?.value}
+						onChange={value => onChange(value, 'time')}
+						onBlur={() => onBlur('time')}
+						error={time?.errorMessage}
+						labelAlign="center"
+					/>
+					<CustomInput
+						placeholder="Escribe cuantos días le dedicaras a la semana"
+						label="Periodicidad"
+						value={periodicity?.value}
+						onChange={value => onChange(value, 'periodicity')}
+						onBlur={() => onBlur('periodicity')}
+						error={periodicity?.errorMessage}
+						labelAlign="center"
+					/>
+					{!isKeyboardShown && (
+						<TouchableOpacity
+							style={newChallengeStyles.button}
+							onPress={handleSubmitForm}
+						>
+							<Text style={newChallengeStyles.textButton}> Guardar reto</Text>
+						</TouchableOpacity>
+					)}
+				</View>
+			</ScrollView>
+		</TouchableWithoutFeedback>
+	);
 };
 export default NewChallenge;
--- app/widgets/shared/CustomInput/CustomInput.js
+++ app/widgets/shared/CustomInput/CustomInput.js
@@ -1,62 +1,62 @@
-import React from "react";
-import { TextInput, View, Text } from "react-native";
-import { customInputStyles } from "./CustomInputStyles";
-import { Picker } from "@react-native-picker/picker";
+import React from 'react';
+import { TextInput, View, Text } from 'react-native';
+import { customInputStyles } from './CustomInputStyles';
+import { Picker } from '@react-native-picker/picker';
 
 const CustomInput = ({
-  placeholder,
-  onChange,
-  onChangeSelect,
-  value,
-  selectValue,
-  error,
-  label,
-  editable = true,
-  onBlur,
-  labelAlign = "left",
-  type = "input",
-  options,
+	placeholder,
+	onChange,
+	onChangeSelect,
+	value,
+	selectValue,
+	error,
+	label,
+	editable = true,
+	onBlur,
+	labelAlign = 'left',
+	type = 'input',
+	options,
 }) => {
-  return (
-    <View style={customInputStyles.wrapper}>
-      <View>
-        {label && (
-          <Text style={[customInputStyles.label, { textAlign: labelAlign }]}>
-            {label}
-          </Text>
-        )}
-        {type === "input" && (
-          <TextInput
-            style={[
-              customInputStyles.input,
-              error && customInputStyles.hasError,
-            ]}
-            placeholder={placeholder}
-            onChangeText={onChange}
-            onBlur={onBlur}
-            value={value}
-            editable={editable}
-          />
-        )}
-        {type === "select" && (
-          <Picker
-            selectedValue={selectValue}
-            style={[customInputStyles.select]}
-            onValueChange={onChangeSelect}
-          >
-            {options.map((option) => (
-              <Picker.Item
-                key={option.value}
-                label={option.name}
-                value={option.value}
-              />
-            ))}
-          </Picker>
-        )}
-        {error && <Text style={customInputStyles.error}>{error}</Text>}
-      </View>
-    </View>
-  );
+	return (
+		<View style={customInputStyles.wrapper}>
+			<View>
+				{label && (
+					<Text style={[customInputStyles.label, { textAlign: labelAlign }]}>
+						{label}
+					</Text>
+				)}
+				{type === 'input' && (
+					<TextInput
+						style={[
+							customInputStyles.input,
+							error && customInputStyles.hasError,
+						]}
+						placeholder={placeholder}
+						onChangeText={onChange}
+						onBlur={onBlur}
+						value={value}
+						editable={editable}
+					/>
+				)}
+				{type === 'select' && (
+					<Picker
+						selectedValue={selectValue}
+						style={[customInputStyles.select]}
+						onValueChange={onChangeSelect}
+					>
+						{options.map(option => (
+							<Picker.Item
+								key={option.value}
+								label={option.name}
+								value={option.value}
+							/>
+						))}
+					</Picker>
+				)}
+				{error && <Text style={customInputStyles.error}>{error}</Text>}
+			</View>
+		</View>
+	);
 };
 
 export default CustomInput;
