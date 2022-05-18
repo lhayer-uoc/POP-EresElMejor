3c6cb4abfeb5d2587b167771ae8aa12a804ae5ea
LHayer-UOC <lhayer@uoc.edu>
2022-05-19 01:16:40 +0200

    wip

--- app/views/ChallengeList/ChallengeList.js
+++ app/views/ChallengeList/ChallengeList.js
@@ -1,73 +1,93 @@
-import React, { useCallback, useState } from "react";
-import Container from "widgets/shared/Container/Container";
-import { View } from "react-native";
+import React, { useCallback, useState } from 'react';
+import { ProgressBar } from '@react-native-community/progress-bar-android';
+import Container from 'widgets/shared/Container/Container';
+import { View, StyleSheet } from 'react-native';
 
-import { challengeListStyles } from "./ChallengeListStyles";
-import List from "app/widgets/shared/List/List";
-import { getChallengesService } from "../../services/getChallengesService";
-import ChallengeCard from "../../widgets/shared/ChallengeCard/ChallengeCard";
-import { useFocusEffect } from "@react-navigation/native";
+import { challengeListStyles } from './ChallengeListStyles';
+import List from 'app/widgets/shared/List/List';
+import { getChallengesService } from '../../services/getChallengesService';
+import ChallengeCard from '../../widgets/shared/ChallengeCard/ChallengeCard';
+import { useFocusEffect } from '@react-navigation/native';
 
 const Item = ({ item, onPress, backgroundColor }) => {
-  return (
-    <ChallengeCard
-      percentage={item.percentage}
-      heading1={item.title}
-      heading2={item.description}
-      onPress={onPress}
-      style={[challengeListStyles.item, backgroundColor]}
-      category={item.category}
-    />
-  );
+	return (
+		<ChallengeCard
+			percentage={item.percentage}
+			heading1={item.title}
+			heading2={item.description}
+			onPress={onPress}
+			style={[challengeListStyles.item, backgroundColor]}
+			category={item.category}
+		/>
+	);
 };
 
-const ChallengeList = (props) => {
-  const [challenges, setChallenges] = useState(null);
-  const [selectedId, setSelectedId] = useState(null);
+const ChallengeList = props => {
+	const [challenges, setChallenges] = useState(null);
+	const [selectedId, setSelectedId] = useState(null);
 
-  const onSelectItem = (item) => {
-    setSelectedId(item.id);
-    props.navigation.navigate("Reto", { itemId: item.id, item });
-  };
+	const onSelectItem = item => {
+		setSelectedId(item.id);
+		props.navigation.navigate('Reto', { itemId: item.id, item });
+	};
 
-  const handleChallenges = async () => {
-    const challenges = await getChallengesService();
-    setChallenges(challenges);
-  };
+	const handleChallenges = async () => {
+		const challenges = await getChallengesService();
+		setChallenges(challenges);
+	};
 
-  useFocusEffect(
-    useCallback(() => {
-      handleChallenges();
-    }, [])
-  );
+	useFocusEffect(
+		useCallback(() => {
+			handleChallenges();
+		}, [])
+	);
 
-  const renderItem = ({ item }) => {
-    const backgroundColor = item.id === selectedId ? "#E3E3E3" : "#FFF";
-    const color = item.id === selectedId ? "white" : "black";
+	const renderItem = ({ item }) => {
+		const backgroundColor = item.id === selectedId ? '#E3E3E3' : '#FFF';
+		const color = item.id === selectedId ? 'white' : 'black';
 
-    return (
-      <Item
-        item={item}
-        onPress={() => onSelectItem(item)}
-        backgroundColor={{ backgroundColor }}
-        textColor={{ color }}
-      />
-    );
-  };
+		return (
+			<Item
+				item={item}
+				onPress={() => onSelectItem(item)}
+				backgroundColor={{ backgroundColor }}
+				textColor={{ color }}
+			/>
+		);
+	};
 
-  return (
-    <Container>
-      <View style={challengeListStyles.container}>
-        <List
-          data={challenges}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-          extraData={selectedId}
-          style={challengeListStyles.list}
-        />
-      </View>
-    </Container>
-  );
+	const styles = StyleSheet.create({
+		example: {
+			marginVertical: 24,
+		},
+	});
+
+	return (
+		<Container>
+			<View style={challengeListStyles.container}>
+				<View style={styles.example}>
+					<ProgressBar
+						styleAttr="Horizontal"
+						color="#2196F3"
+						progress={progress}
+					/>
+					{/* <ProgressBar
+						styleAttr="Horizontal"
+						color="#2196F3"
+						indeterminate={false}
+						progress={0.5}
+					/> */}
+				</View>
+				<List
+					data={challenges}
+					renderItem={renderItem}
+					keyExtractor={item => item.id}
+					extraData={selectedId}
+					style={challengeListStyles.list}
+				/>
+			</View>
+		</Container>
+	);
 };
 
 export default ChallengeList;
