7b89b3b1d3c921ecd379f5efd18eebf0097a92b3
Ferran <ferran.puigmar@gmail.com>
2022-05-21 18:28:40 +0200

    Fix ChnageDetails

--- app/services/getChallengesDetailsService.js
+++ app/services/getChallengesDetailsService.js
@@ -1,12 +1,11 @@
-import { collection, doc, getDoc } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
 import { db } from "../config/db";
 import { challengeToDto } from "./dto/challengeToDto";
 
 export const getChallengeDetailsService = async (id) => {
   try {
-    const docRef = collection(db, "challenges");
+    const docRef = doc(db, "challenges", id);
     const stateQuery = await getDoc(docRef);
-
     return challengeToDto({ id, ...stateQuery.data() });
   } catch (error) {
     return false;
